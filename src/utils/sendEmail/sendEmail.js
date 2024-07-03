346cccf6930d32811022196ccc34d40057add116
martachips <martachips@gmail.com>
2024-07-03 16:35:23 +0200

    included more html content

--- src/utils/sendEmail/sendEmail.js
+++ src/utils/sendEmail/sendEmail.js
@@ -4,11 +4,23 @@ const confirmAttendanceEmail = async (user, eventId) => {
   try {
     const { name, email } = user;
     const htmlContent = `
-   <div class="email-content">
-     <h4 class="email-title">Tu asistencia ha sido confirmada con éxito al evento ${eventId}</h4>
-     <p class="email-p>Relájate, respira, haz tus cosas... porque se va a liar parda</p>
-     <p class="email-p>🤘</p>
-   </div>
+     <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+          
+        </style>
+      </head>
+      <body>
+        <div class="email-content">
+          <h4 class="email-title">Tu asistencia ha sido confirmada con éxito al evento ${eventId}</h4>
+          <p class="email-p>Relájate, respira, haz tus cosas... porque se va a liar parda 😎</p>
+          <p class="email-p>🤘</p>
+        </div>
+      </body>
+    </html>
   `;
     await sendMail(email, 'Asistencia confirmada', htmlContent);
     console.log(`Email sent to ${email}`);
