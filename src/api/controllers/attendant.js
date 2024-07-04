0928b90cc04b727d4a2c7546ced031355bff9cf3
martachips <martachips@gmail.com>
2024-07-04 13:49:41 +0200

    vuelvo a habilitar el envío de correo

--- src/api/controllers/attendant.js
+++ src/api/controllers/attendant.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 const Attendant = require('../models/attendant');
 const Event = require('../models/event');
-// const confirmAttendanceEmail = require('../../utils/sendEmail/sendEmail');
+const confirmAttendanceEmail = require('../../utils/sendEmail/sendEmail');
 
 const getAttendants = async (req, res, next) => {
   try {
@@ -46,12 +46,13 @@ const confirmAssistance = async (req, res, next) => {
       await confirmNewAttendant(name, email, eventId, res);
     }
 
+    console.log('Calling function to send email');
+    confirmAttendanceEmail(req.user || { name, email }, eventId);
+    console.log('Email confirmation sent');
+
     return res
       .status(200)
       .json({ message: `Assistance confirmed successfully` });
-    // console.log('Calling function to send email');
-    // confirmAttendanceEmail(req.user || { name, email }, eventId);
-    // console.log('Email confirmation sent');
   } catch (error) {
     console.error(error);
     return res
